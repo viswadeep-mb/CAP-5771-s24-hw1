f4eb9c12ed09fd23f68b25df1b6c4fcd12cdbfa2
erlebach <gordon.erlebach@gmail.com>
2024-01-08 12:23:58 -0500

    Updated template for Homework1

--- question2.py
+++ question2.py
@@ -1,29 +1,14 @@
-import utils as u
-
-# In test: lower case, no duplicates
-# Handle multiple possible answers
-
-# Use as an example
 def question2_1():
-    answer = ['ratioo', 'discrete', 'quantitative']
     return answer
 
 def question2_2():
-    answer = ['CONtinuous','quantitative', 'ratio']
     return answer
 
 def question2_3():
-    answer = ['continuous', 'quantitative', 'interval']
     return answer 
 
 def question2_4():
-    answer = ['discrete', 'qualitative', 'ordinal']
     return answer
 
 def question2_5():
-    answer = ['discrete', 'qualitative', 'nominal']
     return answer
-
-# change order. Add a word. 
-# Need structural test to pass before answer test pass. 
-
--- question8.py
+++ question8.py
@@ -1,22 +1,11 @@
-import utils as u
-
-# In test: lower case, no duplicates
-# Handle multiple possible answers
-
-# Use as an example
 def question8_1():
-    # Use set of dict['bin1'].value
-    answer = ['x1', 'x2']
     return answer
 
 def question8_2():
-    answer = 'equally similar'
     return answer
 
 def question8_3():
-    answer = 'equally similar'
     return answer 
 
 def question8_4():
-    answer = ['x2', 'y3']
     return answer 
