001e26e70e172aca878e71d184f40e346b8bcfb2
viswadeep-mb <84169100+viswadeep-mb@users.noreply.github.com>
2024-01-29 22:24:25 -0500

    Update question6.py

--- question6.py
+++ question6.py
@@ -15,7 +15,8 @@ def question6_2():
 def question6_3():
     answer = {
         'equal_width': ['Change', 4],
-        'equal_frequency': ['No change', 4]
+        'equal_frequency': ['Change', 4]
     }
-    return answer 
+    return answer
+
 
