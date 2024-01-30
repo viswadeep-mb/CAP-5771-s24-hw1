fae3e7473770c256a975f32384daa78c36a0ab92
viswadeep-mb <84169100+viswadeep-mb@users.noreply.github.com>
2024-01-29 22:29:36 -0500

    Update question6.py

--- question6.py
+++ question6.py
@@ -7,15 +7,15 @@ def question6_1():
 
 def question6_2():
     answer = {
-        'equal_width': ['Change', 9],
+        'equal_width': ['change', 9],
         'equal_frequency': ['No change', 9]
     }
     return answer
 
 def question6_3():
     answer = {
-        'equal_width': ['Change', 4],
-        'equal_frequency': ['Change', 4]
+        'equal_width': ['change', 4],
+        'equal_frequency': ['change', 4]
     }
     return answer
 
