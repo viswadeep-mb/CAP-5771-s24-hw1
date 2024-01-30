40318cab9a2b7404267c1bd0d1ebb38a5da32256
viswadeep-mb <84169100+viswadeep-mb@users.noreply.github.com>
2024-01-29 21:47:01 -0500

    Update question2.py

--- question2.py
+++ question2.py
@@ -7,7 +7,7 @@ def question2_2():
     return answer
 
 def question2_3():
-    answer = ['continuous', 'quantitative', 'ratio']
+    answer = ['continuous', 'quantitative', 'interval']
     return answer 
 
 def question2_4():
